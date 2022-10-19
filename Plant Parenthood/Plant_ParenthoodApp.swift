3e125e97fd79c7935fc693846505820a4bded788
LindsayMorel <lmmorel@umich.edu>
2022-10-19 14:13:16 -0400

    Initial Commit

---
+++ Plant Parenthood/Plant_ParenthoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_ParenthoodApp.swift
+//  Plant Parenthood
+//
+//  Created by Lindsay Morel on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Plant_ParenthoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
