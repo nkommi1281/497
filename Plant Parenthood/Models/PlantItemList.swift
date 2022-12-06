f67a146a3a8974ddcbdd080692856f0a0f396f60
LindsayMorel <lmmorel@umich.edu>
2022-12-05 19:23:25 -0500

    hi sorry

---
+++ Plant Parenthood/Models/PlantItemList.swift
@@ -0,0 +1,26 @@
+//
+//  PlantItemList.swift
+//  Plant Parenthood
+//
+//  Created by Lindsay Morel on 12/5/22.
+//
+
+import Foundation
+
+class PlantItemList: ObservableObject {
+    
+    @Published var itemList: [PlantItem]
+    
+    init(_ itemList: [PlantItem]) {
+        self.itemList = itemList
+    }
+    
+    public func addPlantItem (newPlantItem: PlantItem) {
+        itemList.append(newPlantItem)
+    }
+    
+    public func getPlantItemList() -> [PlantItem] {
+        return itemList
+    }
+    
+}
